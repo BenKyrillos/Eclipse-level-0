765651c5dbe1de4fc35ee5b4da2c043d3bb8e09f
BenKyrillos <Guest@WTS6.local>
2015-06-28 17:25:41 -0700

    great job ben

---
+++ level 0/src/forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 500; i++) {
+
+			System.out.print(i);
+			if (i % 2 == 0) {
+				System.out.println("even");
+			} else {
+				System.out.println("odd ");
+			}
+		}
+	}
+}
